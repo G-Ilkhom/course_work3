2152c2c97600b0e19ffb738c0a92e4d80988fb0b
ILKHOM <gaffarovilkhom@inbox.ru>
2023-12-17 00:54:06 +0300

    Добавлена фильтрация по EXECUTED

--- src/main.py
+++ src/main.py
@@ -6,3 +6,4 @@ from src.utils import get_all_operations
 OPERATIONS_PATH = os.path.join(ROOT_DIR, '', 'operations.json')
 all_operations = get_all_operations(OPERATIONS_PATH)
 all_operations = [operations for operations in all_operations if operations != {}]
+filtered_operations = list(filter(lambda operation: operation["state"] == "EXECUTED", all_operations))
