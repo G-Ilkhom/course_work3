fb8599802db1597b7165d70821c630714439b559
ILKHOM <gaffarovilkhom@inbox.ru>
2023-12-17 00:55:26 +0300

    Добавлена сортировка даты по убыванию

--- src/main.py
+++ src/main.py
@@ -7,3 +7,4 @@ OPERATIONS_PATH = os.path.join(ROOT_DIR, '', 'operations.json')
 all_operations = get_all_operations(OPERATIONS_PATH)
 all_operations = [operations for operations in all_operations if operations != {}]
 filtered_operations = list(filter(lambda operation: operation["state"] == "EXECUTED", all_operations))
+sorted_operations = sorted(filtered_operations[:5], key=lambda operation: operation['date'], reverse=True)
