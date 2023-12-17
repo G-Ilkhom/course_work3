2adaecac4c23f6e38388d08ffa2e8d6135cd0593
ILKHOM <gaffarovilkhom@inbox.ru>
2023-12-17 03:49:53 +0300

    Добавлена функция, считывающая дату и пояснение операции

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,14 @@
-import json
-import datetime
+from json import load
 
-def get_all_operations(path):
-    with open(path, encoding='utf-8') as file:
-        return json.load(file)
 
-def get_formatted_operation(operations: list) -> list:
-    formatted_operation = []
-    for operation in operations:
-        if isinstance(operation, dict):
-            date = datetime.datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%y")
-            description = operation["description"]
-            payer_info = ""
-            payment_method = ""
-            if "from" in operation:
-                payer = operation["from"].split()
-                payment_method = payer.pop(-1)
-                if payer[0].lower() == "счет":
-                    payment_method_from = f"**{payment_method[-4:]}"
-                else:
-                    payment_method_from = f"{payment_method[:4]} {payment_method[4:6]}** **** {payment_method[-4:]}"
-                payer_info = " ".join(payer)
-            recipients = f"{operation['to'].split()[0]} **{operation['to'][-4:]}"
-            operation_amount = f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}"
-            formatted_operation.append(f"""{date} {description}
-{payer_info} {payment_method_from}->{recipients}
-{operation_amount}""")
-    return formatted_operation
+class transaction:
+    def __init__(self, path):
+        with open(path, encoding='UTF-8') as file:
+            self.data = sorted([operation for operation in load(file) if len(operation) > 0 and operation['state'] == 'EXECUTED' and 'Перевод' in operation['description']], key=lambda x: x['date'], reverse=True)
+
+    def get_date_and_description(self, number):
+        date = self.data[number]["date"]
+        description = self.data[number]['description']
+        date = date.split('T')[0]
+        date = '.'.join(date.split('-')[::-1])
+        return f'{date} {description}'
