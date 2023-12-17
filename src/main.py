a2d06186ec63ccf285153f334c298f7224b47c53
ILKHOM <gaffarovilkhom@inbox.ru>
2023-12-17 03:46:17 +0300

    Добавлен фильтр и сортировка

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 from config import ROOT_DIR
 from src.utils import get_all_operations, get_formatted_operation
 
-OPERATIONS_PATH = os.path.join(ROOT_DIR, '', 'operations.json')
+OPERATIONS_PATH = os.path.join(ROOT_DIR, 'src/operations.json')
 all_operations = get_all_operations(OPERATIONS_PATH)
 all_operations = [operations for operations in all_operations if operations != {}]
 filtered_operations = list(filter(lambda operation: operation["state"] == "EXECUTED", all_operations))
